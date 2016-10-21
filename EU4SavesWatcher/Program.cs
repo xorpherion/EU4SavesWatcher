80abb431c8a689d8da4b3165b3537e10aff85ef5
Xorpherion <nogiax@googlemail.com>
2016-10-21 02:11:27 +0200

    initial commit, basic window

---
+++ EU4SavesWatcher/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EU4SavesWatcher
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            using (var window = new Window())
+            {
+                window.Start();
+            }
+        }
+    }
+}
---
+++ EU4SavesWatcher/Window.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EU4SavesWatcher
+{
+    public class Window : IDisposable
+    {
+        private Form window;
+
+        private ListView fileList;
+
+
+
+        public void Dispose()
+        {
+            window.Dispose();
+        }
+
+        public void Start()
+        {
+            Init();
+            Application.Run(window);
+        }
+
+        private void Init()
+        {
+            InitWindow();
+            InitListView();
+        }
+
+        private void InitListView()
+        {
+            fileList = new ListView();
+        }
+
+        private void InitWindow()
+        {
+            window = new Form();
+            window.Text = "EU4SavesWatcher";
+        }
+    }
+}
